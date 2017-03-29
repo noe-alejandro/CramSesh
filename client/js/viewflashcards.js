1066658abb9b34168bdbdde5a0ecd3831ebd83d3
Noe Rojas <noerojas@csu.fullerton.edu>
2017-03-29 14:22:37 -0700

    update

--- client/js/viewflashcards.js
+++ client/js/viewflashcards.js
@@ -7,23 +7,66 @@ var $main = function() {
     success : function(data) {
       // Display the flashcards for the subject selected by the
       // user in the dashboard.
+       var count = 0;
+       var isFront = true;
+       var side = "Front";
 
-      // How to select fields in the deckInfo Object
-      console.log(data.deckInfo);
-      console.log('subject: ' + data.deckInfo.subject);
-      console.log('owner: ' + data.deckInfo.owner);
-      console.log('cards: ' + data.deckInfo.cards);
-      console.log('cards: ' + typeof(data.deckInfo.cards));
-      console.log('cards: ' + data.deckInfo.cards[0].front);
-      console.log('cards: ' + data.deckInfo.cards[0].back);
+       $(".page-header").html("Deck name: "+data.deckInfo.subject);
+       $("#cardnum").html((count+1)+" of "+data.deckInfo.cards.length);
+       $("#cardimage").html(data.deckInfo.cards[count].front);
+       $("#cardside").html(side);
+       //button to flip card sides.
+      $("#flip").on("click",function() {
 
+        if(isFront)
+        {
+          side = "Back";
+          $("#cardimage").html(data.deckInfo.cards[count].back);
+          $("#cardside").html(side);
+          isFront = false;
+        }
+        else
+        {
+          side = "Front";
+          $("#cardimage").html(data.deckInfo.cards[count].front);
+          $("#cardside").html(side);
+          isFront = true;
+        }
+      });
+       //button for next card.
+      $("#nextcard").on("click",function() {
 
+        count++;
+        side = "Front";
+        isFront = true;
 
+        if(count == data.deckInfo.cards.length-1)
+        {
+          $("#nextcard").prop("disabled",true);
+        }
 
+        $("#cardimage").html(data.deckInfo.cards[count].front);
+        $("#prevcard").prop("disabled",false);
+        $("#cardnum").html((count+1)+" of "+data.deckInfo.cards.length);
+        $("#cardside").html(side);
+      });
 
+      //button to go back a card.
+      $("#prevcard").on("click",function() {
+        count--;
+        side = "Front";
+        isFront = true;
 
+        if(count == 0)
+        {
+          $("#prevcard").prop("disabled",true);
+        }
 
-
+        $("#cardimage").html(data.deckInfo.cards[count].front);
+        $("#nextcard").prop("disabled",false);
+        $("#cardnum").html((count+1)+" of "+data.deckInfo.cards.length);
+        $("#cardside").html(side);
+      });
     }
   });
 };
